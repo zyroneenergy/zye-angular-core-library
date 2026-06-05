5e96be3af4f09a1abe54c6f9b5b77ef6b583c78a
Mitesh <miteshprajapati6439@gmail.com>
2026-06-05 14:51:54 +0530

    add styles folders and use as @zye/core

---
+++ projects/core/src/styles/custom_style_variables.ts
@@ -0,0 +1,106 @@
+/*
+  Font Settings
+ */
+
+export const defualtFontFamily = "Nunito";
+export const defualtFontSize = 14;
+export const defualtFontWeight = 400;
+export const chartAxisFontSize = 12;
+export const defualtTooltipTextColor = "#fff";
+export const defualtSeriesTextColor = "#fff";
+export const defualtTooltipBorderColor = "rgba(255,255,255,0.3)";
+
+export const geochartGrey = "#dbe1e8";
+
+export const colorGrey8 = "#cad2dc";
+export const actionColorRed = "#F85529";
+export const actionColorRedLight = "#feddd4";
+export const actionColorOrange = "#FF8C1A";
+export const actionColorOrangeLight = "#ffe8d1";
+export const actionColorYellow = "#FFD965";
+export const actionColorYellowLight = "#fffae8";
+export const actionColorGreen = "#24a148";
+export const actionColorGreenLight = "#6FD88E"; 
+export const actionColorDarkRed = "#D32F2F";
+export const actionColorDarkRedLight = "#ffc1ba";
+
+export const previewLabelColor = "#2A3547";
+
+export const axisLabelColorLight = "#5A6A85";
+export const axisLabelColorDark = "#7C8FAC";
+
+export const barSeriesColor = "#5D87FF";
+export const barSeriesLightColor = "#8eabff";
+export const barSeriesColorEmptyLight = "#DFE5EF";
+export const barSeriesColorEmptyDark = "#5A6A85";
+
+export const tooltipBackgroundColor = "rgba(255, 255, 255, 0)";
+
+export const tooltipBGColorLight1 = "#2A3547";
+export const tooltipBGColorDark1 = "#000000";
+export const tooltipBGColorLight2 = "rgba(42,53,71,0.85)";
+export const tooltipBGColorDark2 = "rgba(0,0,0,0.60)";
+export const toolTipTextColorLight = "#fff";
+
+// Width or Sizes
+export const barSeriesWidth = 10;
+export const chartToolBoxIconSize = 12;
+export const seriesTextColor = "#fff";
+
+export const splitLineLightColor = "#DFE5EF";
+export const splitLineLightColor2 = "#ffffff";
+export const splitLineDarkColor = "#5A6A85";
+
+export const stackBarCenteredLightColor = "#fff";
+export const stackBarCenteredDarkColor = "#2A3547";
+
+export const headerLightColor = "rgba(0, 0, 0, 0.87)";
+export const headerDarkColor = "#EAEFF4";
+export const headerFontWeight = 600;
+export const headerFontSize = 16;
+
+export const fontStyle = "normal";
+
+export const subheaderFontWeight = 400;
+export const subheaderFontSize = 12;
+export const subheaderFontLightColor = "#5A6A85";
+export const subheaderFontDarkColor = "#7C8FAC";
+
+export const legendColor = "#8994a8";
+
+export const chartBackgroundLightColor = "#F2F6FA";
+export const chartBackgroundDarkColor = "#2A3547";
+
+export const mapColorLight = "#FFFFFF";
+export const mapColorDark = "#7C8FAC";
+
+export const backgroundColorLight = "#fff";
+export const backgroundColorDark = "#2A3547";
+
+export const seriesColorLight = "#EAEFF4";
+export const seriesColorDark = "#5A6A85";
+
+export const red500 = "#dd2b0e";
+export const red700 = "#ae1800";
+export const red400 = "#ec5941";
+export const red50 = "#fcf1ef";
+
+export const orange500 = "#ff8c1a";
+export const orange50 = "#ffe8d1";
+export const yellow500 = "#ffbe20";
+export const yellow300 = "#ffd965";
+export const yellow50 = "#fffae8";
+
+export const indigo500 = "#49beff";
+export const indigo50 = "#e8eeff";
+
+export const surface500 = "#33425c";
+export const surface200 = "#eef2f7";
+
+export const heatMapDark = "#539bff";
+export const primary500 = "#5d87ff";
+
+export const greenDark = "#13deb9";
+export const greenLight = "#e6fffa";
+
+export const purple = "#af3296";
